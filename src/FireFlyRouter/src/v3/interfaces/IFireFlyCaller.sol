9ae8f02931905753ea40fc85a5bd76677f2ab7f8
name <fireflyroot88@gmail.com>
2026-01-03 19:13:33 +0800

    code

---
+++ src/FireFlyRouter/src/v3/interfaces/IFireFlyCaller.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Call3Value, Result} from "../utils/FireFlyStructs.sol";
+
+interface IFireFlyCaller {
+    function multicall(
+        Call3Value[] calldata calls,
+        address refundTo,
+        address nftRecipient
+    ) external payable returns (Result[] memory returnData);
+
+    function cleanupErc20s(
+        address[] calldata tokens,
+        address[] calldata recipients,
+        uint256[] calldata amounts
+    ) external;
+
+    function cleanupNative(uint256 amount, address recipient) external;
+}
